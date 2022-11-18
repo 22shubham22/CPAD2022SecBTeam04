65cc19c6d79636db1f6985f761c42b5b71fc2059
Shubham Anand <60786471+22shubham22@users.noreply.github.com>
2022-11-19 02:20:56 +0530

    post api to upload images and first use of tesseract

---
+++ api/upload/upload.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const mongoose = require('mongoose');
+const storage = multer.diskStorage({
+    destination : function (req, file, cb) {
+        cb(null,'uploadedImages')
+    },
+    filename : function (req,file, cb) {
+        const number = Math.floor(Math.random() * 32525345668);
+        cb(null, 'Image' + number.toString() + '.png');
+    }
+});
+const fileFilter = (req ,file ,cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null,true);
+    } else {
+        var error = new Error('Wrong Format');
+        error.status  = 500;
+        cb(error,false);
+    }
+} 
+const upload = multer({storage: storage,fileFilter:fileFilter});
+const tess = require('node-tesseract-ocr');
+function recognize(path) {
+    const config = {
+        lang: "eng",
+        oem: 1,
+        psm: 3,
+      };
+    
+      tess
+      .recognize(path, config)
+      .then((text) => {
+        console.log("Result:", text)
+      })
+      .catch((error) => {
+        console.log(error.message)
+      });
+}
+
+
+
+router.get('/',(req,res,next) => {
+    res.status(200).json({
+        message: 'handling post req to /upload'
+    })
+});
+
+router.post('/', upload.single('productImage'), (req,res,next) => {
+    recognize(req.file.destination+req.file.filename);
+    console.log(req.file);
+    res.status(200).json({
+        message: 'handling post req to /upload'
+    })
+});
+
+module.exports = router;
