e75884c424b934c59df376e3d05c53da120f1767
Shubham Anand <60786471+22shubham22@users.noreply.github.com>
2022-11-27 21:12:27 +0530

    sample image and google vision improv

--- demo.js
+++ demo.js
@@ -1,14 +1,24 @@
+const fs = require('fs');
+
 async function quickstart() {
   // Imports the Google Cloud client library
   const vision = require('@google-cloud/vision');
 
   // Creates a client
-  const client = new vision.ImageAnnotatorClient();
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: "./crossplatform-369812-6c68c2c44e0c.json"
+  });
 
   // Performs label detection on the image file
-const [result] = await client.textDetection('./uploadedImages/Image1836841280.png');
+const [result] = await client.textDetection('./uploadedImages/complex.jpeg');
 const detections = result.textAnnotations;
-console.log('Text:');
-detections.forEach(text => console.log(text));
+// console.log('Text:');
+// detections.forEach(text => console.log(text));
+try {
+  fs.writeFileSync('C:/Users/hp/OneDrive/Documents/GitHub/CPAD2022SecBTeam04/data.txt', JSON.stringify(detections));
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}
 }
 quickstart()
